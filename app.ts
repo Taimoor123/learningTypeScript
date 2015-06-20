8a15f2d959e0492c476da0df0a8794eff81e7f14
Taimoor Tariq <taimoor_t@yahoo.com>
2015-06-19 17:40:26 -0700

    typeScript
    Typescript

--- app.ts
+++ app.ts
@@ -84,6 +84,17 @@ var abc = result("taimoor", "tariq");
 console.log(abc);
 */
 
+//function nameOfFunction(parameter, ....)
+/*
+function Name(firstName: string, ...restOfName: string[]) {
+	return firstName + " " + restOfName.join(" ");
+	
+	
+} 
+
+var emp = Name("asd", "asd", "asd", "asd");
+console.log(emp);
+*/
 
 
 /*
@@ -92,4 +103,51 @@ myType = { id: 2,  name: "Tom", gender: true };// can only assign a type which h
 
 console.log(myType.name);
 */
+
+
+/*
+var add = (a: number, y: number) => {
+	return a + b;
+	
+}
+
+var add1 = (x: number, y: number) => (x + y);
+
+var myFunction = f => {
+	this.x = "x";
+	
+}
+*/
+
+/*
+function myCallBack(text: string) {
+    console.log("inside myCallback " + text);
+}
+
+function callingFunction(initialText: string,
+    callback: (text: string) => void)
+{
+    callback(initialText);
+}
+
+callingFunction("myText", myCallBack);
+*/
+
+//overloads give us type-checked calls
+
+//its customary to order overloads from most specific to least specific
+function add(arg1: string, arg2: string): string;//option 1
+function add(arg1: number, arg2: number): number;//option 2
+function add(arg1: boolean, arg2: boolean): boolean;//option 3
+function add(arg1: any, arg2: any): any {//this is not part of the overload list, so it only has three overloads
+    return arg1 + arg2;
+}
+
+//Calling 'add' with any other parameter types would cause an error except for the three options
+console.log(add(1, 2));
+console.log(add("Hello", "World"));
+console.log(add(true, false));
+
+
+
 
